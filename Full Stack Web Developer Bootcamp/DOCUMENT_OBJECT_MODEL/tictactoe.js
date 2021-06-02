b29a551437e58fb708ed1ce41394db38222e335c
AM KK <andrzej.kapczynski@op.pl>
2021-06-02 22:02:10 +0200

    Document Object Model

---
+++ Full Stack Web Developer Bootcamp/DOCUMENT_OBJECT_MODEL/tictactoe.js
@@ -0,0 +1,24 @@
+//Restart Game
+var restart = document.querySelector("#b");
+//Grabs all the squarees
+var squares = document.querySelectorAll('td');
+
+
+function clearBoard() {
+  for(var i=0; i<squares.length; i++) {
+    squares[i].textContent = "";
+  }
+}
+
+restart.addEventListener('click', clearBoard);
+
+
+function changeMarker() {
+  if (this.textContent === '') this.textContent = 'X';
+  else if (this.textContent === 'X') this.textContent = 'O';
+  else this.textContent = '';
+}
+
+for (i=0; i<squares.length; i++){
+  squares[i].addEventListener('click', changeMarker);
+}
