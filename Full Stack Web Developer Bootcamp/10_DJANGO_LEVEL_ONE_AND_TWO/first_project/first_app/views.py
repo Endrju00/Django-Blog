7955e633892e59ae1340a30f6c796793d96b5268
AM KK <andrzej.kapczynski@op.pl>
2021-06-12 20:18:58 +0200

    Model Forms Exercise

--- Full Stack Web Developer Bootcamp/10_DJANGO_LEVEL_ONE_AND_TWO/first_project/first_app/views.py
+++ Full Stack Web Developer Bootcamp/10_DJANGO_LEVEL_ONE_AND_TWO/first_project/first_app/views.py
@@ -1,6 +1,7 @@
 from django.shortcuts import render
-from django.http import HttpResponse
+# from django.http import HttpResponse
 from first_app.models import Topic, WebPage, AccessRecord, User
+from first_app.forms import NewUserForm
 # Create your views here.
 
 
@@ -10,6 +11,17 @@ def index(request):
     return render(request, 'first_app/index.html', context=date_dict)
 
 def users(request):
-    user_list = User.objects.order_by('first_name')
-    user_dict = {'users': user_list}
-    return render(request, 'first_app/users.html', context=user_dict)
+    # user_list = User.objects.order_by('first_name')
+    # user_dict = {'users': user_list}                                      lvl2
+    # return render(request, 'first_app/users.html', context=user_dict)
+    form = NewUserForm()
+
+    if request.method == 'POST':
+        form = NewUserForm(request.POST)
+
+        if form.is_valid():
+            form.save(commit=True)
+            return index(request)
+        else:
+            print('ERROR form invalid')
+    return render(request, 'first_app/users.html', {'form': form})
